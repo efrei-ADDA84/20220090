98758d90f46f902be47848fbd99674e6b64cffc0
dacruzmathis <mathisdaccruz@gmail.com>
2023-04-28 14:28:23 +0200

    commit TP2

--- tp1.py
+++ tp1.py
@@ -1,14 +1,10 @@
 import os
 import requests
+from flask import Flask, jsonify, request
 
-# Récupérer les variables d'environnement
-lat = os.environ.get('LAT')
-lon = os.environ.get('LONG')
+app = Flask(__name__)
+
 api_key = os.environ.get('API_KEY')
-
-#lat = '31.2504'
-#lon = '-99.2506'
-#api_key = '2161992f8e11948a5c0804c922c44d1b'
 
 def get_url(lat, lon, api_key):
     return f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric'   
@@ -17,6 +13,13 @@ def get_weather(url):
     response = requests.get(url)
     return response.json()  
 
-print(get_weather(get_url(lat, lon, api_key)))
+@app.route('/weather')
+def weather():
+    lat = request.args.get('lat')
+    lon = request.args.get('lon')
+    url = get_url(lat, lon, api_key)
+    weather_data = get_weather(url)
+    return jsonify(weather_data)
 
-#https://api.openweathermap.org/data/2.5/weather?lat=-31.2504&lon=-99.2506&appid=2161992f8e11948a5c0804c922c44d1b
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port= 8081)
