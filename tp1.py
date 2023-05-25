fb81f82ffe51b542636daad1516c8814a1cd21de
dacruzmathis <mathisdaccruz@gmail.com>
2023-05-25 11:51:01 +0200

    cr7

--- tp1.py
+++ tp1.py
@@ -4,20 +4,18 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-api_key = os.environ.get('API_KEY')
-
-def get_url(lat, lon, api_key):
-    return f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric'   
+def get_url(lat, lon):
+    return f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=2161992f8e11948a5c0804c922c44d1b&units=metric'   
 
 def get_weather(url):
     response = requests.get(url)
     return response.json()  
 
-@app.route('/weather')
+@app.route('/')
 def weather():
     lat = request.args.get('lat')
     lon = request.args.get('lon')
-    url = get_url(lat, lon, api_key)
+    url = get_url(lat, lon)
     weather_data = get_weather(url)
     return jsonify(weather_data)
 
